1606a76ec7685dc424127c4f08782f258b861292
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2022-06-12 18:25:41 -0400

    Initial Commit

---
+++ Study Remastered/Study_RemasteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_RemasteredApp.swift
+//  Study Remastered
+//
+//  Created by Brian Masse on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Study_RemasteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
